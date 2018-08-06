fdab3d2b5cb3d94a74409755049ba24230dbc4c6
adithak <adithak59@gmail.com>
2018-08-06 14:54:22 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,24 @@
+public class largest 
+{
+public static void main(String args[])
+{
+int x,y,z;
+System.out.println("enter the numbers");
+System.in = new Scanner(System.in);
+x = in.nextInt();
+y = in.nextInt();
+z = in.nextInt();
+if ( x > y && x > z )
+{
+         System.out.println("first largest.");
+         }
+      else if ( y > x && y > z )
+      {
+         System.out.println("Second largest.");
+}
+else if ( z > x && z > y )
+ {
+ System.out.println("Third  largest.");
+}
+   }
+}
