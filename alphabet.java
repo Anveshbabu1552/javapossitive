1619168ea053059603b33f3e23f7548536034458
adithak <adithak59@gmail.com>
2018-08-06 14:45:50 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,13 @@
+public class alphabet
+{
+  public static void main(String args[])
+ {
+ char b ='d';
+  if( (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z'))
+  {
+  System.out.println(b+"alphabet");
+  }
+  else
+  {
+  System.out.println(b+"number");
+  }}}
